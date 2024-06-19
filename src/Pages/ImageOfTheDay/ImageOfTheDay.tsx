f013e25f2db51d264938ecae2b55fc9a8ac7625f
Makrien <110492469+Makrien@users.noreply.github.com>
2024-06-19 17:10:44 -0300

    Setup APOD API and created ImageOfTheDay page

---
+++ src/Pages/ImageOfTheDay/ImageOfTheDay.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react"
+import { ApodImageryProps, getApodImagery } from "../../services/apodApi/apodApi"
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
+import styles from "./ImageOfTheDayStyles";
+
+const ImageOfTheDay = () => {
+  const [apodData, setApodData] = useState<ApodImageryProps[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchData = async () => {
+    try {
+      const data = await getApodImagery();
+      setApodData(data);
+    } catch (err) {
+      setError("Failed to fetch APOD imagery");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
+  if (error) {
+    return (
+      <View style={styles.errorContainer}>
+        <Text style={styles.errorText}>{error}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={apodData}
+        keyExtractor={(item) => item.date}
+        renderItem={({item}) => (
+          <View style={styles.itemContainer}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Image source={{ uri: item.url }} style={styles.image} />
+            <Text style={styles.explanation}>{item.explanation}</Text>
+            <Text style={styles.copyright}>{item.copyright}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+export default ImageOfTheDay
