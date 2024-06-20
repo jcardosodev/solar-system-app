7bd40cdfab6f97cac17e9d002d242b99654035d7
Marcelo Affonso <162736731+MarceloSAffonso@users.noreply.github.com>
2024-06-20 20:26:24 -0300

    App

--- App.tsx
+++ App.tsx
@@ -22,7 +22,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       {/* <TelaInicial /> */}
-      {/* <SolarSystem/> */}
+      <SolarSystemScreen/>
       {/* <Cadastrar /> */}
       {/* <ImageOfTheDay /> */}
       {/* <Login /> */}
