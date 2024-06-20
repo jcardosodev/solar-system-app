13aa00eddb9d737abe2554fee3a4d3d5876c500f
jcardosodev <jonathanpcardoso24@gmail.com>
2024-06-20 11:46:34 -0700

    Alteracoes no home e navegacao tela inicial

--- App.tsx
+++ App.tsx
@@ -1,9 +1,46 @@
-import { TelaInicial } from "./src/Pages/TelaInicial";
-import { Routes } from "./src/Routes";
+import React, { useEffect, useState } from 'react';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+import * as Font from 'expo-font';
+import  TelaInicial  from './src/Pages/TelaInicial';
+import Routes from './src/Routes';
 
-export default function App() {
-  return (
-    <TelaInicial />
-  );
+const loadFonts = async () => {
+  await Font.loadAsync({
+    'Roboto-Regular': require('../solar-system-app/src/assets/fonts/Roboto-Regular.ttf'),
+    'PlaywriteAUSA-Regular': require('../solar-system-app/src/assets/fonts/PlaywriteAUSA-Regular.ttf'),
+  });
 };
+
+const App = () => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function load() {
+      await loadFonts();
+      setFontsLoaded(true);
+    }
+    load();
+  }, []);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return <Routes />;
+};
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
+
 
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,7 +1,21 @@
-import { NavigationContainer } from "@react-navigation/native"
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import TelaInicial from '../Pages/TelaInicial';
+import HomePage from '../Pages/HomePage';
+import { RootStackParamList } from './types';
 
-export const Routes = () => {
-    return <NavigationContainer>
-    <></>
+const Stack = createStackNavigator<RootStackParamList>();
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="TelaInicial">
+        <Stack.Screen name="TelaInicial" component={TelaInicial} />
+        <Stack.Screen name="HomePage" component={HomePage} />
+      </Stack.Navigator>
     </NavigationContainer>
-}
+  );
+};
+
+export default Routes;
