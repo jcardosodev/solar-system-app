c842b553f904b4224937c99aea6aaa213b32e637
Makrien <110492469+Makrien@users.noreply.github.com>
2024-06-26 11:49:49 -0300

    gitignore teste

--- src/services/UserApi/UserApi.ts
+++ src/services/UserApi/UserApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const userApi = axios.create({
-  baseURL:"http://192.168.1.134:3000",
+  baseURL:"http://localhost:3000",
   headers: {
     'Content-Type': 'application/json'
   }
